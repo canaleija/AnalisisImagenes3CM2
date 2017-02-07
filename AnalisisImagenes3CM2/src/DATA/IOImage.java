1f5de7dacb5ac1811f4216433bdf996b787ceb2e
Roberto Cruz Leija <Roberto Cruz Leija@canaleija>
2017-02-07 13:02:05 -0600

    Abre imagenes diferentes es jinternalsframes diferentes

---
+++ AnalisisImagenes3CM2/src/DATA/IOImage.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DATA;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author Roberto Cruz Leija
+ */
+public class IOImage {
+    
+    public static Image abrirImagen(){
+     
+        try {
+            // definir los fltros de los tipos de imagenes
+            FileNameExtensionFilter filtro =
+                    new FileNameExtensionFilter("Imagenes","png","jpg","bmp");
+            // declarar e inicializar la venta de seleccion
+            JFileChooser seleccion = new JFileChooser();
+            seleccion.addChoosableFileFilter(filtro);
+            // especificamos que solo puede abrir archivos
+            seleccion.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            
+            // ejecutar el selector de archivos
+            int resutlado = seleccion.showOpenDialog(null);
+            // si el evento fue cancelar
+            if (resutlado== JFileChooser.CANCEL_OPTION){
+                return null;
+            }
+            File archivo = seleccion.getSelectedFile();
+            BufferedImage bfi = ImageIO.read(archivo);
+            return bfi.getScaledInstance(bfi.getWidth(),bfi.getHeight(),BufferedImage.TYPE_INT_RGB);
+        } catch (IOException ex) {
+            Logger.getLogger(IOImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+}
